68f5d72bfb1d253374a667dd72245fcafb9faf56
Alienn Cheng <alienncheng@gmail.com>
2019-12-01 00:02:05 +0800

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/NoGo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/NoGo.dir/main.cpp.o"
+  "NoGo"
+  "NoGo.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/NoGo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
